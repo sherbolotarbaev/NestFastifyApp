c909eaf301d8ec745cbd9ed70bd77c344b1733e0
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2024-06-02 07:17:23 +0600

    add: module-alias

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import { fastifyApp } from './common/adapters/fastify.adapter';
 
+import 'module-alias/register';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
